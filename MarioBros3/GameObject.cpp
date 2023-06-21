932d5e358ac1adfbe70f20919ced52fb0e480ecc
4nh3k <21520579@gm.uit.edu.vn>
2023-06-21 23:00:16 +0700

    update screenshot

--- MarioBros3/GameObject.cpp
+++ MarioBros3/GameObject.cpp
@@ -19,23 +19,23 @@ GameObject::GameObject()
 
 void GameObject::RenderBoundingBox()
 {
-	D3DXVECTOR3 p(x, y, 0);
-	RECT rect;
+	//D3DXVECTOR3 p(x, y, 0);
+	//RECT rect;
 
-	LPTEXTURE bbox = Textures::GetInstance()->Get(ID_TEX_BBOX);
+	//LPTEXTURE bbox = Textures::GetInstance()->Get(ID_TEX_BBOX);
 
-	float l,t,r,b; 
+	//float l,t,r,b; 
 
-	GetBoundingBox(l, t, r, b);
-	rect.left = 0;
-	rect.top = 0;
-	rect.right = (int)r - (int)l;
-	rect.bottom = (int)b - (int)t;
+	//GetBoundingBox(l, t, r, b);
+	//rect.left = 0;
+	//rect.top = 0;
+	//rect.right = (int)r - (int)l;
+	//rect.bottom = (int)b - (int)t;
 
-	float cx, cy; 
-	Game::GetInstance()->GetCamera()->GetCamPos(cx, cy);
+	//float cx, cy; 
+	//Game::GetInstance()->GetCamera()->GetCamPos(cx, cy);
 
-	Game::GetInstance()->Draw(x - cx, y - cy, bbox, &rect, BBOX_ALPHA);
+	//Game::GetInstance()->Draw(x - cx, y - cy, bbox, &rect, BBOX_ALPHA);
 }
 
 GameObject::~GameObject()
